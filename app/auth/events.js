898d45b5a8c99b2e598a780a6d8905a5a6ca4371
jbeecy <john.e.beecy@gmail.com>
2021-07-20 16:52:33 -0400

    add if to run event listeners if no winner

--- app/auth/events.js
+++ app/auth/events.js
@@ -84,164 +84,182 @@ const onNewGame = function (event) {
 
 const onBox0 = function (event) {
   event.preventDefault()
-  if ($('#box-0').text() === '') {
-    api.box0()
-      .then(ui.onBox0Success)
-      .catch(ui.onFailure)
-    if (store.currentPlayer === 'X') {
-      store.currentPlayer = 'O'
-    } else {
-      store.currentPlayer = 'X'
-    }
-    $('#box-0').text(store.currentPlayer)
+  if (checkForWin === false) {
+    if ($('#box-0').text() === '') {
+      api.box0()
+        .then(ui.onBox0Success)
+        .catch(ui.onFailure)
+      if (store.currentPlayer === 'X') {
+        store.currentPlayer = 'O'
+      } else {
+        store.currentPlayer = 'X'
+      }
+      $('#box-0').text(store.currentPlayer)
+      checkForWin()
+    } else ($('#message').text('Oops! Pick an empty space.'))
     checkForWin()
-  } else ($('#message').text('Oops! Pick an empty space.'))
-  checkForWin()
-  console.log(store.gameOver)
+    console.log(store.gameOver)
+  }
 }
 
 const onBox1 = function (event) {
   event.preventDefault()
-  if ($('#box-1').text() === '') {
-    api.box1()
-      .then(ui.onBox1Success)
-      .catch(ui.onFailure)
-    if (store.currentPlayer === 'X') {
-      store.currentPlayer = 'O'
-    } else {
-      store.currentPlayer = 'X'
-    }
-    $('#box-1').text(store.currentPlayer)
+  if (checkForWin === false) {
+    if ($('#box-1').text() === '') {
+      api.box1()
+        .then(ui.onBox1Success)
+        .catch(ui.onFailure)
+      if (store.currentPlayer === 'X') {
+        store.currentPlayer = 'O'
+      } else {
+        store.currentPlayer = 'X'
+      }
+      $('#box-1').text(store.currentPlayer)
+      checkForWin()
+    } else ($('#message').text('Oops! Pick an empty space.'))
     checkForWin()
-  } else ($('#message').text('Oops! Pick an empty space.'))
-  checkForWin()
-  console.log(store.gameOver)
+    console.log(store.gameOver)
+  }
 }
 
 const onBox2 = function (event) {
   event.preventDefault()
-  if ($('#box-2').text() === '') {
-    api.box2()
-      .then(ui.onBox2Success)
-      .catch(ui.onFailure)
-    if (store.currentPlayer === 'X') {
-      store.currentPlayer = 'O'
-    } else {
-      store.currentPlayer = 'X'
-    }
-    $('#box-2').text(store.currentPlayer)
+  if (checkForWin === false) {
+    if ($('#box-2').text() === '') {
+      api.box2()
+        .then(ui.onBox2Success)
+        .catch(ui.onFailure)
+      if (store.currentPlayer === 'X') {
+        store.currentPlayer = 'O'
+      } else {
+        store.currentPlayer = 'X'
+      }
+      $('#box-2').text(store.currentPlayer)
+      checkForWin()
+    } else ($('#message').text('Oops! Pick an empty space.'))
     checkForWin()
-  } else ($('#message').text('Oops! Pick an empty space.'))
-  checkForWin()
-  console.log(store.gameOver)
+    console.log(store.gameOver)
+  }
 }
 
 const onBox3 = function (event) {
   event.preventDefault()
-  if ($('#box-3').text() === '') {
-    api.box3()
-      .then(ui.onBox3Success)
-      .catch(ui.onFailure)
-    if (store.currentPlayer === 'X') {
-      store.currentPlayer = 'O'
-    } else {
-      store.currentPlayer = 'X'
-    }
-    $('#box-3').text(store.currentPlayer)
+  if (checkForWin === false) {
+    if ($('#box-3').text() === '') {
+      api.box3()
+        .then(ui.onBox3Success)
+        .catch(ui.onFailure)
+      if (store.currentPlayer === 'X') {
+        store.currentPlayer = 'O'
+      } else {
+        store.currentPlayer = 'X'
+      }
+      $('#box-3').text(store.currentPlayer)
+      checkForWin()
+    } else ($('#message').text('Oops! Pick an empty space.'))
     checkForWin()
-  } else ($('#message').text('Oops! Pick an empty space.'))
-  checkForWin()
-  console.log(store.gameOver)
+    console.log(store.gameOver)
+  }
 }
 
 const onBox4 = function (event) {
   event.preventDefault()
-  if ($('#box-4').text() === '') {
-    api.box4()
-      .then(ui.onBox4Success)
-      .catch(ui.onFailure)
-    if (store.currentPlayer === 'X') {
-      store.currentPlayer = 'O'
-    } else {
-      store.currentPlayer = 'X'
-    }
-    $('#box-4').text(store.currentPlayer)
+  if (checkForWin === false) {
+    if ($('#box-4').text() === '') {
+      api.box4()
+        .then(ui.onBox4Success)
+        .catch(ui.onFailure)
+      if (store.currentPlayer === 'X') {
+        store.currentPlayer = 'O'
+      } else {
+        store.currentPlayer = 'X'
+      }
+      $('#box-4').text(store.currentPlayer)
+      checkForWin()
+    } else ($('#message').text('Oops! Pick an empty space.'))
     checkForWin()
-  } else ($('#message').text('Oops! Pick an empty space.'))
-  checkForWin()
-  console.log(store.gameOver)
+    console.log(store.gameOver)
+  }
 }
 
 const onBox5 = function (event) {
   event.preventDefault()
-  if ($('#box-5').text() === '') {
-    api.box5()
-      .then(ui.onBox5Success)
-      .catch(ui.onFailure)
-    if (store.currentPlayer === 'X') {
-      store.currentPlayer = 'O'
-    } else {
-      store.currentPlayer = 'X'
-    }
-    $('#box-5').text(store.currentPlayer)
+  if (checkForWin === false) {
+    if ($('#box-5').text() === '') {
+      api.box5()
+        .then(ui.onBox5Success)
+        .catch(ui.onFailure)
+      if (store.currentPlayer === 'X') {
+        store.currentPlayer = 'O'
+      } else {
+        store.currentPlayer = 'X'
+      }
+      $('#box-5').text(store.currentPlayer)
+      checkForWin()
+    } else ($('#message').text('Oops! Pick an empty space.'))
     checkForWin()
-  } else ($('#message').text('Oops! Pick an empty space.'))
-  checkForWin()
-  console.log(store.gameOver)
+    console.log(store.gameOver)
+  }
 }
 
 const onBox6 = function (event) {
   event.preventDefault()
-  if ($('#box-6').text() === '') {
-    api.box6()
-      .then(ui.onBox6Success)
-      .catch(ui.onFailure)
-    if (store.currentPlayer === 'X') {
-      store.currentPlayer = 'O'
-    } else {
-      store.currentPlayer = 'X'
-    }
-    $('#box-6').text(store.currentPlayer)
+  if (checkForWin === false) {
+    if ($('#box-6').text() === '') {
+      api.box6()
+        .then(ui.onBox6Success)
+        .catch(ui.onFailure)
+      if (store.currentPlayer === 'X') {
+        store.currentPlayer = 'O'
+      } else {
+        store.currentPlayer = 'X'
+      }
+      $('#box-6').text(store.currentPlayer)
+      checkForWin()
+    } else ($('#message').text('Oops! Pick an empty space.'))
     checkForWin()
-  } else ($('#message').text('Oops! Pick an empty space.'))
-  checkForWin()
-  console.log(store.gameOver)
+    console.log(store.gameOver)
+  }
 }
 
 const onBox7 = function (event) {
   event.preventDefault()
-  if ($('#box-7').text() === '') {
-    api.box7()
-      .then(ui.onBox7Success)
-      .catch(ui.onFailure)
-    if (store.currentPlayer === 'X') {
-      store.currentPlayer = 'O'
-    } else {
-      store.currentPlayer = 'X'
-    }
-    $('#box-7').text(store.currentPlayer)
+  if (checkForWin === false) {
+    if ($('#box-7').text() === '') {
+      api.box7()
+        .then(ui.onBox7Success)
+        .catch(ui.onFailure)
+      if (store.currentPlayer === 'X') {
+        store.currentPlayer = 'O'
+      } else {
+        store.currentPlayer = 'X'
+      }
+      $('#box-7').text(store.currentPlayer)
+      checkForWin()
+    } else ($('#message').text('Oops! Pick an empty space.'))
     checkForWin()
-  } else ($('#message').text('Oops! Pick an empty space.'))
-  checkForWin()
-  console.log(store.gameOver)
+    console.log(store.gameOver)
+  }
 }
 
 const onBox8 = function (event) {
   event.preventDefault()
-  if ($('#box-8').text() === '') {
-    api.box8()
-      .then(ui.onBox8Success)
-      .catch(ui.onFailure)
-    if (store.currentPlayer === 'X') {
-      store.currentPlayer = 'O'
-    } else {
-      store.currentPlayer = 'X'
-    }
-    $('#box-8').text(store.currentPlayer)
+  if (checkForWin === false) {
+    if ($('#box-8').text() === '') {
+      api.box8()
+        .then(ui.onBox8Success)
+        .catch(ui.onFailure)
+      if (store.currentPlayer === 'X') {
+        store.currentPlayer = 'O'
+      } else {
+        store.currentPlayer = 'X'
+      }
+      $('#box-8').text(store.currentPlayer)
+      checkForWin()
+    } else ($('#message').text('Oops! Pick an empty space.'))
     checkForWin()
-  } else ($('#message').text('Oops! Pick an empty space.'))
-  checkForWin()
-  console.log(store.gameOver)
+    console.log(store.gameOver)
+  }
 }
 
 // going to need to repeat event functions for user input of the game
